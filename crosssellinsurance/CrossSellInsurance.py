533a23099aef93795661c855c1e2cd375f4e0ff2
tfmeneghello <tfmeneghello@gmail.com>
2024-07-05 22:55:49 -0300

    first commit_to remote repos

---
+++ crosssellinsurance/CrossSellInsurance.py
@@ -0,0 +1,62 @@
+#%% 9.1 Building class
+#Class
+import pickle
+import pandas as pd
+import numpy as np
+import requests
+import json
+import inflection
+from sklearn import preprocessing as pp
+
+#%%
+class CrossSellInsurance(object):
+    def __init__(self):
+        self.annual_premium_scaler = pickle.load(open('src/features/annual_premium_scaler.pkl', 'rb'))
+        self.age_scaler = pickle.load(open('src/features/age_scaler.pkl', 'rb'))
+        self.gender_scaler = pickle.load(open('src/features/gender_scaler.pkl', 'rb'))
+        self.region_code_scaler = pickle.load(open('src/features/region_code_scaler.pkl', 'rb'))
+        self.sales_channel_scaler = pickle.load(open('src/features/sales_channel_scaler.pkl', 'rb'))
+        self.vintage_scaler = pickle.load(open('src/features/vintage_scaler.pkl', 'rb'))
+
+    def data_cleaning(self, df2):
+        cols_old = df2.columns
+        snakecase = lambda x: inflection.underscore(x)
+        cols_new = list(map(snakecase, cols_old))
+        df2.columns = cols_new
+        return (df2)
+    
+    def feature_engineering(self, df3):
+        df3['vehicle_age'] = ['over_2_years' if i == '> 2 Years' else
+                              'between_1_2_years' if i == '1-2 Year' else
+                              'below_1_year' for i in df3['vehicle_age']]
+        
+        df3['vehicle_damage'] = [1 if i== 'Yes' else 0 for i in df3['vehicle_damage']]
+        return df3
+    
+    def data_preparation(self, df4):
+        #'annual_premium'
+        df4['annual_premium'] = self.annual_premium_scaler.transform(df4[['annual_premium']].values)
+        #'age'
+        df4['age'] = self.age_scaler.transform(df4[['age']].values)
+        #vintage
+        df4['vintage'] = self.vintage_scaler.transform(df4[['vintage']].values)
+        #gender - Label Encoding
+        df4['gender'] = self.gender_scaler.transform(df4['gender'])
+        #'region_code' - Target Encoding / Frequency Encoding
+        df4['region_code'] = self.region_code_scaler.transform(df4[['region_code']], df4['response'])
+        #'vehicle_age' - One Hot Encoding / Order Encoding / Frequency Encoding
+        df4 = pd.get_dummies(df4, prefix='vehicle_age', columns=['vehicle_age'])
+        #'policy_sales_channel' - Target Encoding / Frequency Encoding
+        df4.loc[:, 'policy_sales_channel'] = df4['policy_sales_channel'].map(self.sales_channel_scaler)
+
+        cols_selected = ['age','region_code','previously_insured',
+                         'vehicle_damage','annual_premium',
+                         'policy_sales_channel','vintage']
+        return df4[cols_selected]
+
+    def get_prediction(self, model, original_data, test_data):
+        #prediction
+        pred = model.predict_proba(test_data)
+        # join pred into original date
+        original_data['score'] = pred[:,1]
+        return original_data.to_json( orient='records', date_format = 'iso')
---
+++ handler.py
@@ -0,0 +1,45 @@
+#%%
+#imports
+import pickle
+import os
+import requests
+import json
+import pandas as pd
+from sklearn import ensemble as en
+from sklearn import preprocessing as pp
+from flask import Flask, request, Response
+from crosssellinsurance.CrossSellInsurance import CrossSellInsurance
+
+#%%
+#loading model
+model = pickle.load(open('src/models/model_logreg.pkl', 'rb'))
+
+#initialize API
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def cross_sell_predict():
+    test_json = request.get_json()
+
+    if test_json:    #identificado data
+        if isinstance(test_json, dict):   #unique example
+            test_raw = pd.DataFrame(test_json, index=[0])
+        
+        else:    #multiple example
+            test_raw = pd.DataFrame(test_json, columns=test_json[0].keys())
+        
+        #instantiate class CrossSellInsurance
+        pipeline = CrossSellInsurance()
+        df_pipe = pipeline.data_cleaning(test_raw)
+        df_pipe = pipeline.feature_engineering(df_pipe)
+        df_pipe = pipeline.data_preparation(df_pipe)
+        df_response = pipeline.get_prediction(model, test_raw, df_pipe)
+        
+        return df_response
+    
+    else:
+        return Response('{no data avaible}', status=200, mimetype='application/json')
+
+if __name__=='main':
+    port = os.environ.get('PORT', 5000)
+    app.run(host = '0.0.0.0', port = port)
